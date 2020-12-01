810809bcf570b6f5e0bb59e50c62e4c5b1082df1
Myad Tahajody <myad.tahajody@tretton37.com>
2020-12-01 10:17:22 +0100

    add code formatting

--- 2020/src/day1/ReportRepair.ts
+++ 2020/src/day1/ReportRepair.ts
@@ -3,16 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 async function main() {
-
   let res;
   try {
-
     res = await Axios.get('https://adventofcode.com/2020/day/1/input', {
       headers: {
         // TODO env variable
-        Cookie: `session=${process.env.SESSION}`
+        Cookie: `session=${process.env.SESSION}`,
       },
-      withCredentials: true
+      withCredentials: true,
     });
   } catch (err) {
     throw new Error('When making request:' + err);
@@ -26,28 +24,26 @@ async function main() {
   // Second
   const { i, j, k } = getThreeIndices(all);
   console.info('Answer:', all[i] * all[j] * all[k], 'numbers:', all[i], all[j], all[k]);
-
 }
 
-function getTwoIndices(data: Array<string>): { i: number, j: number } {
+function getTwoIndices(data: Array<string>): { i: number; j: number } {
   for (let i = 0; i < data.length; i++) {
     for (let j = i; j < data.length; j++) {
-
       if (Number(data[i]) + Number(data[j]) === 2020) {
-        return { i, j }
+        return { i, j };
       }
     }
   }
   return { i: -1, j: -1 };
 }
 
-export function getThreeIndices(data: Array<string>): { i: number, j: number, k: number } {
+export function getThreeIndices(data: Array<string>): { i: number; j: number; k: number } {
   for (let i = 0; i < data.length; i++) {
     for (let j = i; j < data.length; j++) {
       for (let k = i; k < data.length; k++) {
         if (Number(data[i]) + Number(data[j]) + Number(data[k]) === 2020) {
-          console.info('returning', i, j, k)
-          return { i, j, k }
+          console.info('returning', i, j, k);
+          return { i, j, k };
         }
       }
     }
@@ -55,4 +51,4 @@ export function getThreeIndices(data: Array<string>): { i: number, j: number, k:
   return { i: -1, j: -1, k: -1 };
 }
 
-process.env.NODE_ENV === "test" ? null : main();
+process.env.NODE_ENV === 'test' ? null : main();
