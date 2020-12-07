8b8346d7f069d80c41de8b180e6fb95994128978
Myad Tahajody <myad.tahajody@tretton37.com>
2020-12-07 22:17:55 +0100

    part 2 wip

--- 2020/src/day7/HandyHaversacks.ts
+++ 2020/src/day7/HandyHaversacks.ts
@@ -1,5 +1,6 @@
 import Axios from 'axios';
 import dotenv from 'dotenv';
+import { Graph } from './Graph';
 
 dotenv.config();
 
@@ -17,19 +18,32 @@ async function main() {
   }
   if (!res?.data) throw new Error('Result was falsy');
   const rows: Array<string> = res.data.split('\n');
+  const graph = new Graph();
   rows.forEach((row) => {
     const [outer, inner] = row?.split(' contain ');
     const children = inner?.split(', ');
-    const parsed = children?.map((child: string) => {
+    const innerColors = children?.map((child: string) => {
       const [count, ...rest] = child.split(' ');
       return {
-        count,
+        weight: count,
         color: `${rest[0]} ${rest[1]}`,
       };
     });
     const outerColor = outer?.replace(' bags', '');
-    console.info({ outerColor, parsed });
+    // console.info({ outerColor, parsed: innerColors });
+    graph.addVertex(outerColor);
+    innerColors?.forEach((vertex) => {
+      // Part 1
+      graph.addParentEdge(outerColor, vertex.color, +vertex.weight);
+      // part 2
+      graph.addChildEdge(outerColor, vertex.color, +vertex.weight);
+    });
   });
+  // graph.print();
+  // console.info(graph.getAncestors('shiny gold').size);
+  console.info(graph.getWeightedDescendants('shiny gold', 1));
+  // console.info(graph.getParents('pale lime'));
+  // console.info(graph.getParents('dim plum'));
 }
 
 process.env.NODE_ENV === 'test' ? null : main();
